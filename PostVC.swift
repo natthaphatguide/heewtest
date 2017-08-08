782bde17e859faff8fe259e4a829833dca81f548
Khem <ppuangp@aca80257.ipt.aol.com>
2017-08-08 23:56:37 +0700

    Fix Google info plist , Add Debug function

--- PostVC.swift
+++ PostVC.swift
@@ -17,6 +17,12 @@ class PostVC: UIViewController {
     
     //ปุ่มจะไปเลือก category ใน array
     
+    
+    
+    
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
@@ -24,6 +30,76 @@ class PostVC: UIViewController {
     }
 
     @IBAction func postBtnPress(_ sender: Any) {
+        /*
+        let userID = Auth.auth().currentUser!.uid
+        
+        guard let brand = brandField.text, brand != "" else {
+            print("GUIDE: Brand must be entered")
+            return
+        }
+        
+        guard let price = priceField.text, price != "" else {
+            print("GUIDE: Price must be entered")
+            return
+        }
+        
+        guard let img = imageAdd.image, imageSelected == true else {
+            print("GUIDE: Image must be selected")
+            return
+        }
+        
+        if let imgData = UIImageJPEGRepresentation(img, 0.2) {
+            let imgUid = NSUUID().uuidString
+            let metadata = StorageMetadata()
+            metadata.contentType = "image/jpeg"
+            
+            DataService.ds.REF_POST_IMAGES.child(imgUid).putData(imgData, metadata: metadata, completion: { (metadata, error) in
+                if error != nil {
+                    print("GUIDE: Unable to upload image to Firebase Storage")
+                } else {
+                    print("GUIDE: Successfully uploaded image to Firebase Storage")
+                    let downloadURL = metadata?.downloadURL()?.absoluteString
+                    if let url = downloadURL {
+                        self.postToFirebase(uid:userID, imgUrl: url)
+                    }
+                }
+            })
+        }
+        */
     }
+    
+    
+    
+//=======================================================
+// Section Fucntion
+//=======================================================
+    /*
+    //Section160: Make post works
+    func postToFirebase(uid:String ,imgUrl:String) {
+        let post: Dictionary<String, Any> = [
+            "uid": uid,
+            "imageUrl": imgUrl,
+            "brand": brandField.text!,
+            "price": priceField.text!          //How to post number??
+        ]
+        
+        let firebasePost = DataService.ds.REF_POSTS.childByAutoId()
+        firebasePost.setValue(post)
+        
+        imageSelected = false
+        imageAdd.image = UIImage(named: "placeholder")
+        brandField.text = ""
+        priceField.text = ""
+        
+        //tableView.reloadData()
+        
+    }
+    
+    
+    */
+    
+    
+    
+    
     
 }
--- SignInVC.swift
+++ SignInVC.swift
@@ -24,9 +24,22 @@ class SignInVC: UIViewController {
         if let email = emailField.text, let password = passwordField.text {
             Auth.auth().signIn(withEmail: email, password: password, completion: { (user, error) in
                 if error == nil {
+                    self.performSegue(withIdentifier: "goToTabberVC", sender: nil)
                     print ("HEEWADMIN: Successfully login with email")
                 } else {
-                    print ("HEEWADMIN: Error to login")
+                    // Create new user if Auth failed.
+                    Auth.auth().createUser(withEmail: email, password: password, completion: { (user, error) in
+                        if error != nil {
+                            print("GUIDE: Unable to authenticate with Firebase using email")
+                        } else {
+                            
+                            if let user = user {
+                                let userData = ["provider": user.providerID, "User": email]
+                                self.completeSignIn(id: user.uid, userData: userData)
+                                print ("HEEWADMIN: Error to login ==> Create new user")
+                            }
+                        }
+                    })
                 }
             })
         }
@@ -59,4 +72,14 @@ class SignInVC: UIViewController {
         })
     }
     
+    // Development only for create user info in database
+    func completeSignIn(id: String, userData: Dictionary<String, String>) {
+        DataService.ds.createFirebaseDBUSER(uid: id, userData: userData)
+        performSegue(withIdentifier: "goToTabberVC", sender: nil)
+    }
+
+    
+    
+    
+
 }
